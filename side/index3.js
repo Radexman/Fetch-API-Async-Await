65951b4b9bebf05bf2df5e5144995c4c61db2774
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-08 17:19:30 +0200

    08.04

--- side/index3.js
+++ side/index3.js
@@ -1,3 +1,4 @@
+const pickerBox = document.querySelector('.picker-box');
 let number = Math.floor(Math.random() * 5 + 1);
 
 const changeSlide = () => {
@@ -14,3 +15,9 @@ const changeSlide = () => {
 };
 
 changeSlide();
+
+pickerBox.addEventListener('click', (e) => {
+	let number = e.target.textContent;
+	let file = `<img src="images/pic${number}.jpg" class="slide" />`;
+	document.querySelector('.slide-box').innerHTML = file;
+});
