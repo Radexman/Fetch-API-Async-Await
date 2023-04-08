5394e43590662c4e145856da81e19b4f968c8d4b
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-08 11:07:38 +0200

    08.04

--- side/index.js
+++ side/index.js
@@ -1,12 +1,35 @@
-const displayTimer = () => {
-	setInterval(() => {
-		const date = new Date();
-		const currentHour = date.getHours();
-		const currentMinute = date.getMinutes();
-		const currentSeconds = date.getSeconds();
-		const fullHour = `${currentHour}:${currentMinute}:${currentSeconds}`;
-		document.querySelector('#timer').textContent = fullHour;
-	}, 1000);
+// Get value of input on button click
+// Chceck if value is bigger, lesser or equal to 0
+// Display propper message on the screen
+
+const checkBtn = document.querySelector('#check-btn');
+const p = document.createElement('p');
+
+const checkValue = (e) => {
+	e.preventDefault();
+	const inputValue = document.querySelector('#number-value').value;
+	const number = Number(inputValue);
+
+	p.innerHTML = '';
+	checkNumber(number);
 };
 
-displayTimer();
+const checkNumber = (num) => {
+	let msg;
+
+	if (num > 0) {
+		msg = 'Number is bigger than 0';
+	} else if (num < 0) {
+		msg = 'Number is less than 0';
+	} else if (num === 0) {
+		msg = 'Number is 0';
+	} else {
+		msg = 'Please enter a number';
+	}
+
+	const pText = document.createTextNode(msg);
+	p.appendChild(pText);
+	document.querySelector('form').appendChild(p);
+};
+
+checkBtn.addEventListener('click', checkValue);
