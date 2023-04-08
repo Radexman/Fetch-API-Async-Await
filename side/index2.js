c1a7d6b764a9b1bb1ec0163cdac92abfbd42dc94
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-08 16:11:55 +0200

    Refactor to multiple functions

--- side/index2.js
+++ side/index2.js
@@ -1,27 +1,31 @@
 const showRangeButton = document.querySelector('#show-range-button');
 const div = document.createElement('div');
+const arr = [];
 
 const showNumberRange = (e) => {
-	e.preventDefault();
 	const firstNumber = document.querySelector('#first-number').value;
 	const lastNumber = document.querySelector('#last-number').value;
+	e.preventDefault();
 
-	if (firstNumber > lastNumber) {
+	createArray(firstNumber, lastNumber);
+
+	div.innerHTML = '';
+	displayContent(arr);
+};
+
+const createArray = (numOne, numTwo) => {
+	if (numOne > numTwo) {
 		alert('First number has to be smaller than second number');
 		return;
 	} else {
-		const arr = [];
-		let x = Number(firstNumber);
-		let y = Number(lastNumber);
+		let x = Number(numOne);
+		let y = Number(numTwo);
 
 		let i = x;
 		while (x <= y) {
 			arr.push(x);
 			x++;
 		}
-
-		div.innerHTML = '';
-		displayContent(arr);
 	}
 };
 
