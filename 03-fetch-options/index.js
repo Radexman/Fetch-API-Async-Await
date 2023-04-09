2585374e68d9b57464e0bb5021d5923aeba80873
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-09 15:40:43 +0200

    09.04

--- 03-fetch-options/index.js
+++ 03-fetch-options/index.js
@@ -1,15 +1,18 @@
 // ==================== 03 Fetch Options =================== //
 
-const createPost = ({ title, body }) => {
+const createPost = (post) => {
 	fetch('https://jsonplaceholder.typicode.com/posts', {
 		method: 'POST',
 		body: JSON.stringify({
-			title,
-			body,
+			title: post.title,
+			body: post.body,
 		}),
 		headers: {
 			'Content-Type': 'application/json',
-			token: 'abd123',
 		},
-	});
+	})
+		.then((res) => res.json())
+		.then((data) => console.log(data));
 };
+
+createPost({ title: 'Post One', body: 'Post one body' });
