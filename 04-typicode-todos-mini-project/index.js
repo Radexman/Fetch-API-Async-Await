034cc0d3030cdb33c8ea24cc68e7c875ce827e1a
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-18 19:16:35 +0200

    18.04

--- 04-typicode-todos-mini-project/index.js
+++ 04-typicode-todos-mini-project/index.js
@@ -1,60 +1,25 @@
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
 const getTodos = () => {
-	fetch(apiUrl + '?_limit=5')
+	fetch(apiUrl + '?_limit=8')
 		.then((res) => res.json())
 		.then((data) => {
-			renderTodos(data);
+			data.forEach((task) => {
+				const div = document.createElement('div');
+				div.appendChild(document.createTextNode(task.title));
+				div.setAttribute('data-id', task.id);
+
+				if (task.completed) {
+					div.classList.add('done');
+				}
+
+				document.querySelector('#todo-list').appendChild(div);
+			});
 		});
 };
 
-const renderTodos = (tasks) => {
-	tasks.forEach((task) => {
-		const div = document.createElement('div');
-		const divText = document.createTextNode(task.title);
-		div.appendChild(divText);
-		div.setAttribute('data-id', 'task.id');
-
-		if (task.completed) {
-			div.classList.add('done');
-		}
-
-		document.querySelector('#todo-list').appendChild(div);
-	});
+const appInit = () => {
+	window.addEventListener('DOMContentLoaded', getTodos);
 };
 
-const renderItemToDOM = (data) => {
-	const div = document.createElement('div');
-	const divText = document.createTextNode(data.title);
-	div.appendChild(divText);
-	div.setAttribute('data-id', 'task.id');
-	document.querySelector('#todo-list').appendChild(div);
-};
-
-const createTodo = (e) => {
-	e.preventDefault();
-
-	const newTodo = {
-		title: e.target.firstElementChild.value,
-		completed: false,
-	};
-
-	fetch(apiUrl, {
-		method: 'POST',
-		body: JSON.stringify(newTodo),
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	})
-		.then((res) => res.json())
-		.then((data) => {
-			renderItemToDOM(data);
-		});
-};
-
-const init = () => {
-	document.addEventListener('DOMContentLoaded', getTodos());
-	document.querySelector('#todo-form').addEventListener('submit', createTodo);
-};
-
-init();
+appInit();
