d9768781ec087c6d36cd095f0d8d7e458a6f760d
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-18 19:43:34 +0200

    18.04

--- 04-typicode-todos-mini-project/index.js
+++ 04-typicode-todos-mini-project/index.js
@@ -18,8 +18,42 @@ const getTodos = () => {
 		});
 };
 
+const postTodo = (e) => {
+	e.preventDefault();
+	let inputValue = e.target.firstElementChild.value;
+
+	if (!inputValue) {
+		alert('Please type in todo');
+	} else {
+		const task = {
+			title: inputValue,
+			completed: false,
+		};
+
+		fetch(apiUrl, {
+			method: 'POST',
+			body: JSON.stringify(task),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				renderTodoToDOM(data);
+			});
+	}
+};
+
+const renderTodoToDOM = (task) => {
+	const div = document.createElement('div');
+	div.appendChild(document.createTextNode(task.title));
+	div.setAttribute('data-id', task.id);
+	document.querySelector('#todo-list').appendChild(div);
+};
+
 const appInit = () => {
 	window.addEventListener('DOMContentLoaded', getTodos);
+	document.querySelector('#todo-form').addEventListener('submit', postTodo);
 };
 
 appInit();
