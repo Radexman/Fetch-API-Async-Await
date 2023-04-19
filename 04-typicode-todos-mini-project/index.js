255a09c52023f90bfbda7c27fe4e6d3d6a1d63ab
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-19 08:48:45 +0200

    19.04

--- 04-typicode-todos-mini-project/index.js
+++ 04-typicode-todos-mini-project/index.js
@@ -57,16 +57,37 @@ const toggleCompleted = (e) => {
 	if (e.target.classList.contains('todo')) {
 		e.target.classList.toggle('done');
 
-		updateTodo(e.target.dataset.id);
+		updateTodo(e.target.dataset.id, e.target.classList.contains('done'));
 	}
 };
 
-const updateTodo = (id, completed) => {};
+const updateTodo = (id, completed) => {
+	fetch(`${apiUrl}/${id}`, {
+		method: 'PUT',
+		body: JSON.stringify({ completed }),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+};
+
+const deleteTodo = (e) => {
+	if (e.target.classList.contains('todo')) {
+		const id = e.target.dataset.id;
+
+		fetch(`${apiUrl}/${id}`, {
+			method: 'DELETE',
+		})
+			.then((res) => res.json())
+			.then(() => e.target.remove());
+	}
+};
 
 const appInit = () => {
 	window.addEventListener('DOMContentLoaded', getTodos);
 	document.querySelector('#todo-form').addEventListener('submit', postTodo);
 	document.querySelector('#todo-list').addEventListener('click', toggleCompleted);
+	document.querySelector('#todo-list').addEventListener('dblclick', deleteTodo);
 };
 
 appInit();
