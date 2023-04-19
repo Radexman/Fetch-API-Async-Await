ecb0195cf8110079a7528cbcb7350b0ef96ee622
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-19 15:09:31 +0200

    19.04

--- 06-async-await/index.js
+++ 06-async-await/index.js
@@ -1,1 +1,45 @@
 // ====================== Async Await ===================== //
+
+const promise = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve({ name: 'John', age: 20 });
+	}, 1000);
+});
+
+// promise.then((data) => console.log(data));
+
+async function getPromise() {
+	const res = await promise;
+	console.log(res);
+}
+
+// getPromise();
+
+// Fetch API with async/await
+const getUsers = async () => {
+	const res = await fetch('https://jsonplaceholder.typicode.com/users');
+	const data = await res.json();
+
+	console.log(data);
+};
+
+// getUsers();
+
+// Fetch API with thanables
+
+const getUsersTwo = () => {
+	fetch('https://jsonplaceholder.typicode.com/users')
+		.then((res) => res.json())
+		.then((data) => console.log(data));
+};
+
+// getUsersTwo();
+
+const getPosts = async () => {
+	const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
+	const data = await res.json();
+
+	console.log(data);
+};
+
+getPosts();
