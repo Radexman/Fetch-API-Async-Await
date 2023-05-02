93232201b382569766f46366b071a1c40f9b40fa
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-05-02 19:57:23 +0200

    02.05

--- 04-typicode-todos-mini-project/index.js
+++ 04-typicode-todos-mini-project/index.js
@@ -4,11 +4,45 @@ const getTodos = async (url) => {
 	const res = await fetch(`${url}?_limit=5`);
 	const data = await res.json();
 
-	console.log(data);
+	data.forEach((todo) => addTodoToDOM(todo));
+};
+
+const addTodoToDOM = (todo) => {
+	const div = document.createElement('div');
+	div.appendChild(document.createTextNode(todo.title));
+	div.setAttribute('data-id', todo.id);
+
+	if (todo.completed) {
+		div.classList.add('done');
+	}
+
+	document.querySelector('#todo-list').appendChild(div);
+};
+
+const createTodo = async (e) => {
+	e.preventDefault();
+
+	const post = {
+		title: e.target.firstElementChild.value,
+		completed: false,
+	};
+
+	const res = await fetch(apiURL, {
+		method: 'POST',
+		body: JSON.stringify(post),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+
+	const data = await res.json();
+
+	addTodoToDOM(data);
 };
 
 const appInit = () => {
-	document.body.addEventListener('DOMContentLoaded', getTodos(apiURL));
+	document.addEventListener('DOMContentLoaded', getTodos(apiURL));
+	document.querySelector('#todo-form').addEventListener('submit', createTodo);
 };
 
 appInit();
