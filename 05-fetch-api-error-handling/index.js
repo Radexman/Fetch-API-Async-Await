c57a7c5db99706935dbe9a3b4d9b57024d11d950
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-05-03 18:33:02 +0200

    03.05

--- 05-fetch-api-error-handling/index.js
+++ 05-fetch-api-error-handling/index.js
@@ -6,8 +6,23 @@
 // 	})
 // 	.then(() => console.log('sucess'));
 
-fetch('http://httpstat.us/404')
-	.then((res) => {
-		return res;
-	})
-	.then(() => console.log('sucess'));
+// fetch('http://httpstat.us/404')
+// 	.then((res) => {
+// 		console.log(res.ok);
+// 		return res;
+// 	})
+// 	.then(() => console.log('sucess'))
+// 	.catch((error) => {
+// 		console.log(error);
+// 	});
+
+// Catch runs on a network error
+// fetch('http://hello123.net')
+// 	.then((res) => {
+// 		console.log(res.status);
+// 		return res;
+// 	})
+// 	.then(() => console.log('sucess'))
+// 	.catch((error) => {
+// 		console.log(error);
+// 	});
--- 06-async-await/index.js
+++ 06-async-await/index.js
@@ -1,54 +1,34 @@
-// ====================== Async Await ===================== //
-
+// ===================== 06 Async Await ==================== //
 const promise = new Promise((resolve, reject) => {
 	setTimeout(() => {
-		let error = false;
-
-		if (!error) {
-			resolve('Success');
-		} else {
-			reject('Failure');
-		}
+		resolve({ name: 'John', age: 20 });
 	}, 1000);
 });
 
-// promise.then((data) => console.log(data)).catch((error) => console.log(error));
+// promise.then((data) => console.log(data));
 
 async function getPromise() {
 	const res = await promise;
-
 	console.log(res);
 }
 
 // getPromise();
 
-async function getUsers() {
-	const res = await fetch('https://jsonplaceholder.typicode.com/users');
+const getUsers = async () => {
+	const res = await fetch('https://jsonplaceholder.typicode.com/users?_limit=5');
 	const data = await res.json();
 
-	console.log(data);
-}
-
-// getUsers();
-
-const getUsersTwo = async () => {
-	const res = await fetch('https://jsonplaceholder.typicode.com/users?_limit=5');
-	const users = await res.json();
-
-	renderUsersToDOM(users);
+	renderUsers(data);
 };
 
-const renderUsersToDOM = (users) => {
+const renderUsers = (users) => {
 	users.forEach((user) => {
 		const div = document.createElement('div');
-		div.classList.add('user');
 		div.appendChild(document.createTextNode(user.name));
+		div.classList.add('user');
+
 		document.querySelector('#output').appendChild(div);
 	});
 };
 
-const initApp = () => {
-	document.querySelector('button').addEventListener('click', getUsersTwo);
-};
-
-initApp();
+document.querySelector('button').addEventListener('click', getUsers);
