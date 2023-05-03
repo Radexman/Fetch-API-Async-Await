0dd407422ab600e8c10da2edb111abcff6291566
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-05-03 13:02:15 +0200

    03.05

--- 04-typicode-todos-mini-project/index.js
+++ 04-typicode-todos-mini-project/index.js
@@ -9,6 +9,7 @@ const getTodos = async (url) => {
 
 const addTodoToDOM = (todo) => {
 	const div = document.createElement('div');
+	div.classList.add('todo');
 	div.appendChild(document.createTextNode(todo.title));
 	div.setAttribute('data-id', todo.id);
 
@@ -22,14 +23,14 @@ const addTodoToDOM = (todo) => {
 const createTodo = async (e) => {
 	e.preventDefault();
 
-	const post = {
+	const newTodo = {
 		title: e.target.firstElementChild.value,
 		completed: false,
 	};
 
 	const res = await fetch(apiURL, {
 		method: 'POST',
-		body: JSON.stringify(post),
+		body: JSON.stringify(newTodo),
 		headers: {
 			'Content-Type': 'application/json',
 		},
@@ -40,9 +41,31 @@ const createTodo = async (e) => {
 	addTodoToDOM(data);
 };
 
+const toggleCompleted = (e) => {
+	if (e.target.classList.contains('todo')) {
+		e.target.classList.toggle('done');
+
+		updateTodo(e.target.dataset.id, e.target.classList.contains('done'));
+	}
+};
+
+const updateTodo = async (id, completed) => {
+	const res = await fetch(`${apiURL}/${id}`, {
+		method: 'PUT',
+		body: JSON.stringify({ completed }),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+	const data = await res.json();
+
+	console.log(data);
+};
+
 const appInit = () => {
 	document.addEventListener('DOMContentLoaded', getTodos(apiURL));
 	document.querySelector('#todo-form').addEventListener('submit', createTodo);
+	document.querySelector('#todo-list').addEventListener('click', toggleCompleted);
 };
 
 appInit();
