395511bb8247dd09a1b5e322b2e7758667860deb
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-05-03 13:29:53 +0200

    03.05

--- 05-fetch-api-error-handling/index.js
+++ 05-fetch-api-error-handling/index.js
@@ -4,38 +4,10 @@
 // 	.then((res) => {
 // 		return res;
 // 	})
-// 	.then(() => console.log('Success'));
+// 	.then(() => console.log('sucess'));
 
-// Test wirh response.ok
-// fetch('http://httpstat.us/404')
-// 	.then((res) => {
-// 		if (!res.ok) {
-// 			throw new Error('Request Failed');
-// 		}
-// 		return res;
-// 	})
-// 	.then(() => console.log('Success'))
-// 	.catch((error) => console.log(error));
-
-// Catch runs on a network error
-// fetch('http://hello123.net')
-// 	.then((res) => {
-// 		return res;
-// 	})
-// 	.then(() => console.log('Success'))
-// 	.catch((error) => console.log(error));
-
-// Check for specific code
-fetch('http://httpstat.us/200')
+fetch('http://httpstat.us/404')
 	.then((res) => {
-		if (res.status === 404) {
-			throw new Error('Not Found');
-		} else if (res.status === 500) {
-			throw new Error('Server Error');
-		} else if (res.status !== 200) {
-			throw new Error('Request Failed');
-		}
 		return res;
 	})
-	.then(() => console.log('Success'))
-	.catch((error) => console.log(error));
+	.then(() => console.log('sucess'));
